7a9e658fd28b1635bd86b7e0bc3929491717dd19
Adam McMahon <obelesk411@users.noreply.github.com>
2016-10-17 17:35:53 -0600

    feat: add more styles

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
 	<div class="main">
 		<div class="container">
 			<?php if (have_posts()) : ?>
+				
 				<?php while(have_posts()) : the_post(); ?>
-					<h3><?php the_title(); ?></h3>
-					<div class="meta">
-						Created By <?php the_author(); ?> on <?php the_date(); ?> at <?php the_time(); ?>
-					</div>
-					<?php the_content(); ?>
+					<article class="post">
+						<h3>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h3>
+						<div class="meta">
+							Created By <?php the_author(); ?> on <?php the_date(); ?> at <?php the_time(); ?>
+						</div>
+						<?php the_content(); ?>
+						<br>
+						<a class="button" href="<?php the_permalink(); ?>">
+							Read More
+						</a>
+					</article>
 				<?php endwhile; ?>
 			<?php else : ?>
 				<?php echo wpautop('Sorry, no posts here.'); ?>
